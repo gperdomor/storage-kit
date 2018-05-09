d25adaffa3f16a78e0a9c8a61eb8a19958f976f6
Gustavo Perdomo <gperdomor@gmail.com>
2018-05-09 18:04:58 -0300

    Update to Vapor 3.0.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "StorageKit", targets: ["StorageKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "StorageKit", dependencies: ["Vapor"]),
